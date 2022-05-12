b205cf4a370f748c5c6245c3ef51c1f28b0fd8e4
yelowt <tom.dangleterre@gmail.com>
2022-05-12 17:32:29 +0200

    creating and styling the landing page

---
+++ script.js
@@ -0,0 +1,17 @@
+function reveal() {
+const reveals = document.querySelectorAll(".reveal");
+
+for (var i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 80;
+
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+    }
+    }
+    
+}
+
+window.addEventListener("scroll", reveal);
+
