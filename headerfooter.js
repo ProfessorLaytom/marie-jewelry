6be0177325ff9cd62407234636344b70a0d71f28
yelowt <tom.dangleterre@gmail.com>
2022-08-17 15:56:22 +0200

    added celia in footer desktop

--- headerfooter.js
+++ headerfooter.js
@@ -60,7 +60,7 @@ const makeHeaderFooter = (path) => {
         code += '\t\t\t</div>\n';
         code += '\t\t</div>\n';
         code += '\t\t<p>© MARIE-ALEXANDRINE YVERNAULT 2022</p>\n';
-        code += '\t\t<p>Made with ❤️ by <a href="https://github.com/ProfessorLaytom">Tom Dangleterre</a></p>\n';
+        code += '\t\t<p>Made with ❤️ by <a href="https://github.com/ProfessorLaytom">Tom Dangleterre</a> & Celia Bidaud</p>\n';
         return code;
     }
     
